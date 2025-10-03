241ac03054de0004bc0b5bb587ff5ce5e9b6203f
Shahil <sahilaryan9211@gmail.com>
2025-10-03 10:38:44 +0530

    fix:removed font awesome as cdn and imported it directly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 // Polyfill Node globals required by some libraries (gray-matter) when running in the browser
-import { Buffer } from 'buffer'
-import process from 'process'
-import './index.css'
-import App from './App.jsx'
+import { Buffer } from "buffer";
+import process from "process";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./index.css";
+import App from "./App.jsx";
 
 // Ensure runtime globals are present for dependencies that expect Node-like environment
-if (typeof globalThis !== 'undefined') {
-  if (!globalThis.Buffer) globalThis.Buffer = Buffer
-  if (!globalThis.process) globalThis.process = process
+if (typeof globalThis !== "undefined") {
+  if (!globalThis.Buffer) globalThis.Buffer = Buffer;
+  if (!globalThis.process) globalThis.process = process;
 }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
